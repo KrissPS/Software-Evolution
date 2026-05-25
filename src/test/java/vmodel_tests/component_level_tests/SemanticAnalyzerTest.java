05d425c59ab0ecc8f9476eb4ff9de6bb2b821011
SolomonFanta <56479535+SolomonFanta@users.noreply.github.com>
2026-05-25 19:34:07 +0200

    feat(tests): component level tests init

---
+++ src/test/java/vmodel_tests/component_level_tests/SemanticAnalyzerTest.java
@@ -0,0 +1,26 @@
+package vmodel_tests.component_level_tests;
+
+import ast.ASTNode;
+import ast.BuildASTVisitor;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.jupiter.api.Test;
+import parser.BabyCobolLexer;
+import parser.BabyCobolParser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SemanticAnalyzerTest {
+
+    // @Test
+    // void testSemanticValidTypes() {
+    //     // TODO: add test 
+    //     return true;
+    // }
+
+    // @Test
+    // void testSemanticErrorInvalidTypeBinding() {
+    //     // TODO: add test
+    //     return true;
+    // }
+}
